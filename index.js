b86947ef62be811b07e0bd5773f10a9a776b18a4
Abdirahman abass diis <abdirahmanfarmajo7786@gmail.com>
2023-06-16 13:37:08 +0300

    first assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let response;
+  if (distance <= 400) {
+    response = 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    response = 'No can do.';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
